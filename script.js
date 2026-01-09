cc7f0247aa96f2d598d2a0b254020641ef332bc9
saddiggzlayer-blip <saddiggzlayer@gmail.com>
2026-01-09 03:51:25 +0000

    refactor app component and update styles

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// Function to flip the card when clicked
 function flipCard() {
     document.getElementById('card').classList.toggle('flipped');
 }
 
-// Function to open/close the editing menu
 function toggleEditor() {
     const panel = document.getElementById('editor-panel');
     panel.classList.toggle('hidden');
 }
 
-// Function to process user input and update the card
 function updateID() {
-    // 1. Get all input values
+    // 1. Get Values
     const fname = document.getElementById('fname-input').value;
     const sname = document.getElementById('sname-input').value;
     const gname = document.getElementById('name-input').value;
@@ -22,28 +19,27 @@ function updateID() {
     const expiry = document.getElementById('expiry-input').value;
     const imgInput = document.getElementById('image-input');
 
-    // 2. Update Text on Card
+    // 2. Update Front
     if(fname) document.getElementById('display-fname').innerText = fname.toUpperCase();
-    if(sname) document.getElementById('display-sname').innerText = sname;
+    if(sname) document.getElementById('display-sname').innerText = sname.toUpperCase();
     if(gname) document.getElementById('display-name').innerText = gname.toUpperCase();
     if(sex) document.getElementById('display-sex').innerText = sex;
-    if(idnum) document.getElementById('display-id-num').innerText = idnum;
-    
-    // 3. Format and update dates
-    if(bday) document.getElementById('display-date').innerText = bday;
+    if(idnum) document.getElementById('display-id-num').innerText = idnum.toUpperCase();
+    if(bday) document.getElementById('display-date').innerText = bday.replace(/-/g, '.');
+
+    // 3. Update Back
     if(issue) document.getElementById('display-issue').innerText = issue.replace(/-/g, '/');
     if(expiry) document.getElementById('display-expiry').innerText = expiry.replace(/-/g, '/');
 
     // 4. Update Photo
     if (imgInput.files && imgInput.files[0]) {
         const reader = new FileReader();
-        reader.onload = function(e) { 
-            document.getElementById('id-photo').src = e.target.result; 
+        reader.onload = function(e) {
+            document.getElementById('id-photo').src = e.target.result;
         };
         reader.readAsDataURL(imgInput.files[0]);
     }
 
-    // 5. Hide panel and alert success
     toggleEditor();
-    alert("Мэдээлэл шинэчлэгдлээ! (Identity Updated)");
+    alert("Мэдээлэл шинэчлэгдлээ!");
 }
